d9c4794ebf36a7476b68e721ce783eb642cf97fb
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-11-09 00:50:15 +0000

    SP

---
+++ helloWorldProgram/helloWorld.java
@@ -0,0 +1,8 @@
+class helloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World ");
+    }
+// This is my first program in the java programming language :)
+}

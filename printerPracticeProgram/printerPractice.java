205f97806235fbe36a17d80b58e1d97e29f819a4
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-11-13 14:37:36 +0000

    SP

---
+++ printerPracticeProgram/printerPractice.java
@@ -0,0 +1,18 @@
+class printerPractice
+{
+    public static void main(String args[])
+    {
+        System.out.println(' ');
+        System.out.println("Welcome User");
+        System.out.println(' ');
+        System.out.println(' ');
+        System.out.println("User's Name: Seth Peterson");
+        System.out.println(' ');
+        System.out.println(' ');
+        System.out.println("User's Age: 16 years");
+        System.out.println(' ');
+        System.out.println(' ');
+        System.out.println("Thank you user for picking Printer101");
+        System.out.println(' ');
+     }
+}

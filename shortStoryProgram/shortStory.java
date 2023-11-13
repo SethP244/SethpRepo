5ef66d1bb84cee11e7ef4e5f620a6366e1d73987
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-11-13 15:11:47 +0000

    SP

---
+++ shortStoryProgram/shortStory.java
@@ -0,0 +1,15 @@
+class shortStory
+{
+public static void main(String args[])
+{
+System.out.println(' ');
+System.out.println("Title: The Life of a Programer");
+System.out.println(' ');
+System.out.println("A man is sitting at his computer desk in a dark room. The blue hue of his computer illuminating the surrounding walls of the room. ");
+System.out.println("The man looks withered and exhausted. He is continueously typing and his eyes are staring into the ether of his computer.");
+System.out.println("The man suddenly stops and says,'It only took me 9 hours, but I finally debugged my program!'");
+System.out.println("The man looks at his phone, the time is 3:27 A.M.. The man skulks back to his bed to restart his monotonous cycle of existence.");
+System.out.println(' ');
+}
+//Bugs and errors are NOT fun 
+}

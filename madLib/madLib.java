431882df9299117345839ef7c59b5e97fbd72c1d
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-11-29 15:18:07 +0000

    SP

---
+++ madLib/madLib.java
@@ -0,0 +1,30 @@
+class madLip 
+{
+    public static void main(String args[])
+    {
+        String adjectiveOne;
+        adjectiveOne = "sad ";
+        String nounOne;
+        nounOne = "Kanye East";
+        String nounTwo;
+        nounTwo = " Ryan Renolds";
+        String adjectiveTwo;
+        adjectiveTwo = "Confused ";
+        String verbOne;
+        verbOne = " CRying";
+        String verbTwo;
+        verbTwo = "Booming ";
+        String adverbOne;
+        adverbOne = "Mightly ";
+        String exclamation;
+        exclamation = "HUZAH ";
+        String nounPlural;
+        nounPlural = " Germans";
+
+    System.out.println("A " + nounOne + " was eating " + nounPlural + ". Then, the " + nounOne + " " + verbOne + " towards the " );
+    System.out.println(" " + adjectiveOne + " " + nounTwo + ". The two were very " + adjectiveTwo + " so they ate more " + nounPlural + " " + adverbOne + ".");
+    System.out.println(" " + exclamation + " said the " + nounOne + " it was late to a doctor's appointment, so it " + verbTwo + " all the way home.");
+    }
+
+
+}

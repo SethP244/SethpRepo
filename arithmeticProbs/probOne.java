c9a2d097a57658158eed14732e64a8efb5a3017c
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-12-11 14:58:21 +0000

    Sp

---
+++ arithmeticProbs/probOne.java
@@ -0,0 +1,15 @@
+class probOne
+{
+    public static void main(String args[])
+    {
+        int num1 = 20;
+        int num2 = 57;
+        int num3 = num1 + num2;
+        
+
+        System.out.println("This is the result " + num3);
+
+
+    }
+
+}

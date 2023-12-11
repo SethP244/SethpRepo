94f44a081b6442d6f1caac56655cdcacf1bca25a
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-12-11 15:10:53 +0000

    SP

---
+++ arithmeticProbs/probTwo.java
@@ -0,0 +1,14 @@
+class probTwo
+{
+    public static void main(String args[])
+    {
+       int currentYear = 2023;
+       int birthday = 16;
+       int birthYear = currentYear - birthday;
+
+       System.out.println("Riley's birthyear is " + birthYear);
+
+
+    }
+
+}

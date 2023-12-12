ee5ad9bd7e39036c5a22fce6066aad49ee1579db
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-12-12 20:08:28 +0000

    SP

---
+++ arithmeticProbs/probThree.java
@@ -0,0 +1,17 @@
+class probOne
+{
+    public static void main(String args[])
+    {
+        int side1 = 4;
+        int side2 = 4;
+        int area = side1 * side2;
+
+
+        System.out.println("The rectangle has an area of " + area);
+
+
+    }
+
+
+
+}

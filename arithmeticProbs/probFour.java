0bfe0fe982e799498358fe97ea949b1f33061f73
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-12-12 20:14:52 +0000

    SP

---
+++ arithmeticProbs/probFour.java
@@ -0,0 +1,22 @@
+class probFour
+{
+    public static void main(String args[])
+    
+    {
+        int sideA = 4;
+        int sideB = 6;
+        int sideC = 10;
+        int sideD = 9;
+        int perimeter = sideA + sideB + sideC + sideD;
+
+
+        System.out.println("The perimeter is " + perimeter + " of a trapezoid.");
+
+
+
+
+
+
+    }
+
+} 

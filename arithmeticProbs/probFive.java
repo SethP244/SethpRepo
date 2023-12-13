623b8b1b6f8889ae29d67002870044a401200bba
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-12-13 15:06:42 +0000

    SP

---
+++ arithmeticProbs/probFive.java
@@ -0,0 +1,15 @@
+class probFive
+{
+    public static void main(String args[])
+    {
+        double ogPrice = 20.0;
+        double discountRate = 0.2;
+        double discount = ogPrice * discountRate;
+        double discountPrice = ogPrice - discount;
+
+
+    System.out.println("The chocolate bar was $20 and had a discount at 20%.");
+    System.out.println("The discounted price is $" + discountPrice);
+    }
+
+}    

81854b7938dbe91fd9fc11b26a4b7b158de81175
SethP244 <150174289+SethP244@users.noreply.github.com>
2023-12-13 15:17:50 +0000

    SP

---
+++ arithmeticProbs/probSix.java
@@ -0,0 +1,15 @@
+class probSix
+{
+    public static void main(String args[])
+    {
+        int num1 = 10;
+        int num2 = 65;
+        int num3 = 78;
+        int sumNumber = num1 + num2 + num3;
+        int averageNumber = sumNumber/ 3;
+
+
+        System.out.println("My grade is a " + averageNumber);
+    }
+
+}

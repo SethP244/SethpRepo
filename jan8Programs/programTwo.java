093cdee4cd9b981777cd8452548ef45412fe8d7d
SethP244 <150174289+SethP244@users.noreply.github.com>
2024-01-10 15:58:12 +0000

    SP

---
+++ jan8Programs/programTwo.java
@@ -0,0 +1,24 @@
+class jan8Programs 
+{
+    public static void main(String args[])
+    { 
+    int num1 = 8;
+    int squNum = num1 * num1;
+    int finalNum = squNum/2;
+
+     System.out.println(' ');
+    System.out.println(' ');
+    System.out.println(' ');
+    System.out.println("The answer is " + finalNum);
+    System.out.println(' ');
+    System.out.println(' ');
+    System.out.println(' ');
+
+
+    }
+    
+    
+    
+    
+    
+    }

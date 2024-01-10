149ca0c63c91c7b0941b3eebe6a789ffcfacd012
SethP244 <150174289+SethP244@users.noreply.github.com>
2024-01-10 16:38:29 +0000

    Sp

---
+++ jan8Programs/programThree.java
@@ -0,0 +1,19 @@
+class jan8Programs 
+{
+    public static void main(String args[])
+    {
+        double pi = 3.141592653589793238462643;
+        double radius = 5.0;
+        double height = 8.0;
+        double vol = Math.pow(radius, 2.0) * pi * height;
+        double surArea = 2 * pi * radius * (radius + height);
+        System.out.println("The suface area is " +surArea);
+        System.out.println("The volume is " + vol);
+
+
+
+
+
+    }
+    
+    }

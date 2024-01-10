4e55eb666f503403b4307debd64fde5851a2e69a
SethP244 <150174289+SethP244@users.noreply.github.com>
2024-01-10 15:51:37 +0000

    SP

---
+++ jan8Programs/programOne.java
@@ -0,0 +1,25 @@
+class jan8Programs 
+{
+    public static void main(String args[])
+    {
+        int num1 = 28;
+        int num2 = 65;
+        int num3 = 99;
+        int num4 = 86;
+        int sum = num1 + num2 + num3 + num4;
+        int finalGrade = sum/4;
+
+
+    System.out.println(' ');
+    System.out.println(' ');
+    System.out.println(' ');
+    System.out.println("Your final grade is " + finalGrade);
+    System.out.println(' ');
+    System.out.println(' ');
+    System.out.println(' ');
+
+
+
+    }
+    
+}

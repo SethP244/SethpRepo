71ca31b885984fae555d2321d7995a5a12696847
SethP244 <150174289+SethP244@users.noreply.github.com>
2024-01-24 16:25:37 +0000

    SP

---
+++ jan8Programs/proFour.java
@@ -0,0 +1,18 @@
+class jan8Programs 
+{
+    public static void main(String args[])
+    {
+int wholeNum = 89;
+int dI = wholeNum/9;
+int kmI = wholeNum %9;
+
+
+
+
+System.out.println( "89 divided by 9 is "+ dI+ "," );
+System.out.println("but with a remainder of "+ kmI);
+
+
+    }
+    
+    }
